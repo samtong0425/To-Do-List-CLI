2995b1399064530b6fc76e5cc1fc7fd934060550
samtong0425 <samtong0425@gmail.com>
2025-05-25 20:01:08 +0800

    Chnage the task id to str based.

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import sys
 import os
 import json
 
-database = {}
+Database = {}
 
 try:
     with open("database.json", "r") as file:
-        saved_database = json.load(file)
-    for i, item in enumerate(saved_database.items(), 1):
-        database.update({i: item[1]})
+        database = json.load(file)
 except FileNotFoundError:
     pass
 except NameError:
@@ -17,8 +15,7 @@ except json.decoder.JSONDecodeError:
     pass
 
 
-def display_list():
-    global database
+def display_list(database):
     os.system("clear")
     print(f"{'=' * 20} TO DO LIST {'=' * 20} ")
     for n, task in database.items():
@@ -33,22 +30,22 @@ def display_list():
 '''
 
 def add_task(new_task, database):
-    new_id = len(database) + 1
-    database[new_id] = {"item": new_task, "status": "incomplete"}
-    return database
+    new_id_int = len(database) + 1
+    new_id_str = str(new_id_int)
+    database[new_id_str] = {"item": new_task, "status": "incomplete"}
 
-def toggle_task_status(id, database):
-    if database[id]['status'] == 'incomplete':
-        database[id].update({"status": "complete"})
+def toggle_task_status(id_str, database):
+    if database[id_str]['status'] == 'incomplete':
+        database[id_str].update({"status": "complete"})
     else:
-        database[id].update({"status": "incomplete"})
+        database[id_str].update({"status": "incomplete"})
 
-def delete_task(id, database):
-    deleted_item = database.pop(id)
+def delete_task(id_str, database):
+    deleted_item = database.pop(id_str)
     update_database = {}
 
     for i, item in enumerate(database.items(), 1):
-        update_database.update({i: item[1]})
+        update_database.update({str(i): item[1]})
 
     database.clear()
     database.update(update_database)
@@ -58,10 +55,9 @@ def delete_task(id, database):
 ===============Sub-Menu UI===============
 '''
 
-def new_task():
-    global database
+def new_task_menu(database):
     while True:
-        display_list()
+        display_list(database)
         print("\n----- ADD NEW TASK -----")
         new_task = input("ADD: Enter new task (or 'q' to return):").strip()
         print("-" * 25)
@@ -73,68 +69,51 @@ def new_task():
             continue
         else:
             add_task(new_task, database)
-            display_list()
+            display_list(database)
             print(f"\n{new_task.capitalize()} is added.")
 
 
-def mark_complete():
-    global database
+def mark_complete_menu(database):
     while True:
-        
-        display_list()
+        display_list(database)
         print("\n----- MARK TASK COMPLETE -----")
         prompt = "MARK: Enter task ID (or 'q' to return): "
-        id = input(prompt).strip().lower()
+        id_str = input(prompt).strip().lower()
         print("-" * 30)
 
-        if id in ["q", "quit"]:
+        if id_str in ["q", "quit"]:
             break
 
-        try:
-            id = int(id)
-        except ValueError:
-            print("Invalid Input")
-            input("Press Enter to continue...")
-            continue
-
-        if id in database.keys():
-            toggle_task_status(id, database)
-            display_list()
-            item, status = database[id].values()
-            print(f"\n{id}: {item.capitalize()} - Status: {status.capitalize()}")
+        if id_str in database.keys():
+            toggle_task_status(id_str, database)
+            display_list(database)
+            item, status = database[id_str].values()
+            print(f"\n{id_str}: {item.capitalize()} - Status: {status.capitalize()}")
         else:
             print("Your selected item is not exit.")
             input("Press Enter to continue...")
             continue
 
 
-def del_item():
-    global database
+def del_item_menu(database):
     while True:
-        display_list()
+        display_list(database)
         print("\n----- DELETE TASK -----")
         delete_item = (
             input("DELETE: Enter task ID (or 'q' to return): ").strip().lower()
         )
         if delete_item in ["q", "quit"]:
             break
-        try:
-            delete_item = int(delete_item)
-        except ValueError:
-            print("Invalid Input")
-            input("Press Enter to continue...")
-            continue
 
         if delete_item in database.keys():
-            id = int(delete_item)
             del_comfirm = (
-                input(f"Do you confirm delete {database[id]['item']}? (y)es or no: ")
+                input(f"Do you confirm delete {database[delete_item]['item']}? (y)es or no: ")
                 .strip()
                 .lower()
             )
             if del_comfirm in ["yes", "y"]:
-                deleted_item = delete_task(id, database)
-                display_list()
+                deleted_item = delete_task(delete_item, database)
+                display_list(database   )
                 print(f"\n{deleted_item['item'].capitalize()} is deleted.")
 
         else:
@@ -150,18 +129,18 @@ def main():
     global database
 
     while True:
-        display_list()
+        display_list(database)
         print(f"\n{'=' * 21} MAIN MENU {'=' * 20} ")
         prompt = "\nCommand: (a)dd, (m)ark, (d)elete, (h)lep, (q)uit: "
         command = input(prompt).strip().lower()
         if command in ["a", "add"]:
-            new_task()
+            new_task_menu(database)
 
         elif command in ["m", "mark"]:
-            mark_complete()
+            mark_complete_menu(database)
 
         elif command in ["d", "delete"]:
-            del_item()
+            del_item_menu(database)
 
         elif command in ["h", "help"]:
             os.system("clear")
