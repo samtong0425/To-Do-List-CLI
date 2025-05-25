df7069fd155df48514c476f7325cde3798671a07
samtong0425 <samtong0425@gmail.com>
2025-05-25 20:20:30 +0800

    Adding test to all business logic

--- test_main.py
+++ test_main.py
@@ -1,12 +1,36 @@
 import main
 
+
 def test_add_task():
     database = {}
     new_task = "Buy milk"
     main.add_task(new_task, database)
     print(database)
     assert len(database) == 1
-    assert '1' in database
-    assert database['1'] == {"item": "Buy milk", "status": "incomplete"}    
+    assert "1" in database
+    assert database["1"] == {"item": "Buy milk", "status": "incomplete"}
 
-# test_add_task()
+
+def test_toggle_task_status():
+    database = {"1": {"item": "Grocery shopping", "status": "incomplete"}}
+    id = "1"
+    assert database[id]["status"] == "incomplete"
+    main.toggle_task_status(id, database)
+    assert database[id]["status"] == "complete"
+    main.toggle_task_status(id, database)
+    assert database[id]["status"] == "incomplete"
+
+
+def test_delete_task():
+    database = {
+        "1": {"item": "Grocery shopping", "status": "incomplete"},
+        "2": {"item": "Pay bills", "status": "incomplete"},
+        "3": {"item": "Schedule doctor's appointment", "status": "incomplete"},
+        "4": {"item": "Walk the dog", "status": "incomplete"},
+        "5": {"item": "Read a book", "status": "incomplete"},
+    }
+    id_str = "3"
+    main.delete_task(id_str, database)
+    assert len(database) == 4
+    assert "5" not in database
+    assert database["3"] == {"item": "Walk the dog", "status": "incomplete"}
